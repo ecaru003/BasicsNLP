08306490e1b72cc2181bfb1caaccb328aef82597
Edrey <ecaru003@fiu.edu>
2020-09-28 17:00:58 -0400

    Added main functions and imports

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import json
 import requests
 import nltk
+from nltk import sent_tokenize
+from nltk import word_tokenize
+from nltk.probability import  FreqDist
+from nltk.corpus import stopwords
+
+nltk.download("punkt")
+nltk.download("stopwords")
+
 
 
 url = "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key="
