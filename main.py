a16ab98cf271e63ec09b52482c078c26a7750295
Edrey <ecaru003@fiu.edu>
2020-09-28 17:07:48 -0400

    First working request

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import nltk
+import main_functions
 from nltk import sent_tokenize
 from nltk import word_tokenize
 from nltk.probability import  FreqDist
@@ -10,5 +11,14 @@ nltk.download("punkt")
 nltk.download("stopwords")
 
 
+api_key_dict = main_functions.read_from_file("JSON_Files/api_key.json")
 
-url = "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key="
+api_key = api_key_dict["my_key"]
+
+url = "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=" + api_key
+
+
+#print(url)
+response = requests.get(url).json()
+
+main_functions.save_to_file(response,"JSON_Files/response.json")
