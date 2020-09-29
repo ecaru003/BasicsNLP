1fe7a7b6e89b01196149a7f2b622e5532c1be854
Edrey <ecaru003@fiu.edu>
2020-09-29 06:14:10 -0400

    Words processing possible

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from nltk import sent_tokenize
 from nltk import word_tokenize
 from nltk.probability import  FreqDist
 from nltk.corpus import stopwords
+from pprint import pprint
 
-nltk.download("punkt")
-nltk.download("stopwords")
+#nltk.download("punkt")
+#nltk.download("stopwords")
 
 
 api_key_dict = main_functions.read_from_file("JSON_Files/api_key.json")
@@ -19,6 +20,53 @@ url = "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=" + api_key
 
 
 #print(url)
-response = requests.get(url).json()
+#response = requests.get(url).json()
 
-main_functions.save_to_file(response,"JSON_Files/response.json")
+#main_functions.save_to_file(response,"JSON_Files/response.json")
+
+
+my_articles = main_functions.read_from_file("JSON_Files/response.json")
+
+str1=""
+
+for i in my_articles["results"]:
+    str1 = str1 + i["abstract"]
+
+#print(str1)
+sentences = sent_tokenize(str1)
+
+words = word_tokenize(str1)
+
+#print(words)
+
+#fdist = FreqDist(words)
+
+#pprint(fdist.most_common(10))
+
+words_no_punc= []
+
+for w in words:
+    if w.isalpha():
+        words_no_punc.append(w.lower())
+
+#pprint(words_no_punc)
+
+fdist2 = FreqDist(words_no_punc)
+
+#pprint(fdist2.most_common(10))
+
+stopwords = stopwords.words("english")
+
+#print(stopwords)
+
+clean_words = []
+
+for w in words_no_punc:
+    if w not in stopwords:
+        clean_words.append(w)
+
+pprint(len(clean_words))
+
+fdist3 = FreqDist(clean_words)
+
+pprint(fdist3.most_common(10))
